cc337909ff33e2f251ca4c8655cab5e34b412a24
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-06-05 21:28:16 -0600

    Just pushing on laptop

--- copyWin.sh
+++ copyWin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf /mnt/c/Users/7406672/AppData/Local/nvim/init.vim
-cp ./win.vim  /mnt/c/Users/7406672/AppData/Local/nvim/init.vim
+rm -rf /mnt/c/Users/Stealth/AppData/Local/nvim/init.vim
+cp ./win.vim  /mnt/c/Users/Stealth/AppData/Local/nvim/init.vim
