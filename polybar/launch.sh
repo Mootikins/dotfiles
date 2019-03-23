01b6a0d31fb816e923b790cb93cc393cfa748351
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-03-23 08:46:34 -0600

    Various dunstrc changes, minor polybar change

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,7 +4,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# Launch any bars
 sleep 1; polybar example
 
 echo "Bars launched..."
