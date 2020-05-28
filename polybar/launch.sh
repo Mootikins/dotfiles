5e2b137dd9698a419233c0eebdc5208d544a43e5
Matthew Krohn <matthew.d.krohn@gmail.com>
2020-05-27 18:42:59 -0600

    Better Polybar Handling and new Display Layout Script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,6 +5,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch any bars
-sleep 1; polybar example
+sleep 1
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+done
 
 echo "Bars launched..."
