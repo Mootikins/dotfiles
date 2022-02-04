aeba7300b57d6d1a395a6548955bfb718c317c6e
Matthew Krohn <matthew.krohn@ravenind.com>
2022-02-04 11:09:03 -0600

    Better diff highlights

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -5,3 +5,10 @@ augroup BgHighlight
     autocmd WinLeave * set nocul
 augroup END
 ]] 
+
+vim.cmd [[
+highlight DiffText guibg=#2222CC guifg=none
+highlight DiffAdd guibg=#009900 guifg=none
+highlight DiffDelete guibg=#DD0000 guifg=none
+highlight DiffChange guibg=#000044 guifg=none
+]]
