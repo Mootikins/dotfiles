8228be18e7d7d3a5e89d0746d0109877f74cc887
Matthew Krohn <matthew.krohn@ravenind.com>
2022-02-11 12:48:57 -0600

    Fix slow nvim-tree opening from conflicting gitsigns mappings

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -15,10 +15,7 @@ require('gitsigns').setup {
 		map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
 		map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
 		map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-		map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
-		map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
-		map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-		map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+		map('n', '<leader>bt', '<cmd>Gitsigns toggle_current_line_blame<CR>')
 
 		map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
 		map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,5 +1,8 @@
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+	view = { width = 45 }
+})
 
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
+
 map('n', '<leader>t', '<Cmd>NvimTreeToggle<CR>', opts)
