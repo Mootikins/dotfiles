c0db37c1f5dc80cd3a2addd4d73adddf05427ca6
Matthew Krohn <matthew.d.krohn@gmail.com>
2022-04-16 14:58:34 -0500

    Change deprecated maintained to all

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 require('nvim-treesitter.configs').setup {
 	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-	ensure_installed = "maintained",
+	ensure_installed = "all",
 
 	-- Install languages synchronously (only applied to `ensure_installed`)
 	sync_install = false,
 
 	-- List of parsers to ignore installing
-	ignore_install = {},
+	ignore_install = {'phpdoc'},
 
 	highlight = {
 		-- `false` will disable the whole extension
