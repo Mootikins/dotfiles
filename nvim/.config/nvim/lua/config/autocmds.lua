fbac5640e8a0ce6e9ccd5922da1a1a29ec9f6cb0
Matthew Krohn <matthew.krohn@cnhind.com>
2024-01-17 16:20:01 -0600

    Fix nvim p2

---
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,18 @@
+api = vim.api
+
+local ft_extensions = api.nvim_create_augroup("FiletypeExtensions", { clear = false })
+
+api.nvim_create_autocmd({ "BufEnter", "BufNew" }, {
+  pattern = "*.lst",
+  command = "set filetype=dosini",
+})
+
+api.nvim_create_autocmd({ "BufEnter", "BufNew" }, {
+  pattern = "*.conf.sample",
+  command = "set filetype=bitbake",
+})
+
+api.nvim_create_autocmd({ "BufEnter", "BufNew" }, {
+  pattern = "*.conf",
+  command = "set filetype=bitbake",
+})
